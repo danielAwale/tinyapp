e357a37d4afcce6bfca4301829290c66116c2105
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-25 19:51:44 +0000

    Add HTML

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { application } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; 
@@ -13,6 +14,10 @@ app.get("/", (req,res) => {
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>HELLO <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
