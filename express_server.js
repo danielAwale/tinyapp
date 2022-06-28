0fec23aedba8208de9dc2fa807de7f6777e9e012
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-28 16:19:27 +0000

    add post to /urls

--- express_server.js
+++ express_server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; 
 
+
 app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
@@ -14,9 +19,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("ok");
 })
 
 app.listen(PORT, () => {
