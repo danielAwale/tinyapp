9a278f2e67a93046dac3cc1612b6648e2e03e803
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-29 17:54:08 +0000

    add delete button

--- express_server.js
+++ express_server.js
@@ -47,6 +47,12 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
