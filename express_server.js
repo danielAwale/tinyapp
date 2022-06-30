fb252ddbfc79324978f31fabb5e91542a32f6fc8
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-30 23:18:47 +0000

    logout button is working

--- express_server.js
+++ express_server.js
@@ -1,13 +1,20 @@
+//EXPRESS
 const express = require("express");
+//COOKIE!
 const cookieParser = require("cookie-parser");
+//Initialize express
 const app = express();
+//Just a general PORT
 const PORT = 8080; 
 
 //ejs view engine
 app.set('view engine', 'ejs');
+//start using cookieparser
 app.use(cookieParser());
 
+//body parser
 const bodyParser = require("body-parser");
+//use bodyparser!!
 app.use(bodyParser.urlencoded({extended: true}));
 
 //function that generates a random string
@@ -34,30 +41,36 @@ app.get('/urls', (req, res) => {
 
 //RENDERS THE URLS/NEW 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
+//not sure what this is for
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]}
   res.render("urls_show", templateVars);
 });
 
+//Generating a small url for our new added long url
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
 });
 
+//NOT SURE WHAT THIS IS FOR??
 app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 });
 
+//DELETING A SPECIFIC DATA FROM OUR URLDATABASE
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
   res.redirect('/urls');
 });
 
+//EDITING THE LONG URL 
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const longURL = req.body.longURL
@@ -66,11 +79,18 @@ app.post("/urls/:id", (req, res) => {
 
 });
 
+//cookie first one
 app.post("/login", (req, res) => {
-  res.cookie("Username", `${req.body.username}`);
+  res.cookie("username", `${req.body.username}`);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect("/urls");
 })
 
+//SERVER IS LISTENING .LISTEN!!! :)
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
