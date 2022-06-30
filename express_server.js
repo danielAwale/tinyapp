ddf4f83c7d16b9b2bd91720ae94035c1f3323278
Rahul (Daniel) Awale <danielmadhukarawale@gmail.com>
2022-06-30 04:54:00 +0000

    install cookie-parser and set up app.use

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; 
 
 
 app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
